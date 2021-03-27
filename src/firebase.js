e8ce2c1c1cead06a4315c952af4132376d086027
Solomon <solomonmeresa8@gmail.com>
2021-03-27 16:17:07 -0700

    add react-router

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyDdGwzuzlLi6xz5NfHf2YswLzxnkHNoNHk',
+	authDomain: 'slaco-7d313.firebaseapp.com',
+	projectId: 'slaco-7d313',
+	storageBucket: 'slaco-7d313.appspot.com',
+	messagingSenderId: '937631837619',
+	appId: '1:937631837619:web:02351e905604ecee44afc6',
+};
